e6f583700c466f3d0c715c171f3b2b2281c0fbf5
rock <insfocus@gmail.com>
2019-11-26 15:01:45 +0800

    regenerate a new vue project in browerjs

--- browserjs/examples/vue-demo/src/views/About.vue
+++ browserjs/examples/vue-demo/src/views/About.vue
@@ -1,33 +1,5 @@
 <template>
   <div class="about">
-    <h1>Hello {{ name }} {{ times }} times.</h1>
-
-    <a-button type="primary" @click="sayHello">Click Me</a-button>
-    <br /><br />
-    <a-button type="primary">Second Button</a-button>
+    <h1>This is an about page</h1>
   </div>
 </template>
-
-<script>
-import NewWorkflow from "@/workflows/workflow1";
-
-export default {
-  name: "About",
-  data() {
-    return {
-      name: "",
-      times: 0,
-    };
-  },
-  methods: {
-    sayHello: function() {
-      const wf = new NewWorkflow();
-      wf.run().then(() => {
-        this.name = "Rock";
-        this.times += 1;
-        console.info("wf exectued");
-      });
-    }
-  }
-};
-</script>
